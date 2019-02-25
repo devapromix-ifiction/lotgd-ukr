62644ea15688943f0729432e88380db15c03ad00
devapromix <bees@meta.ua>
2019-02-25 12:31:09 +0200

    Переклав українською "Рубін".

---
+++ lotgd/modules/findgem.php
@@ -0,0 +1,40 @@
+<?php
+// translator ready
+// mail ready
+// addnews ready
+function findgem_getmoduleinfo(){
+	$info = array(
+		"name"=>"Знайти Рубін",
+		"version"=>"1.1",
+		"author"=>"Eric Stevens",
+		"category"=>"Forest Specials",
+		"download"=>"core_module",
+	);
+	return $info;
+}
+
+function findgem_install(){
+	module_addeventhook("forest", "return 100;");
+	module_addeventhook("travel", "return 20;");
+	return true;
+}
+
+function findgem_uninstall(){
+	return true;
+}
+
+function findgem_dohook($hookname,$args){
+	return $args;
+}
+
+function findgem_runevent($type,$link)
+{
+	global $session;
+	output("`^Ви знайшли в багнюці великий `%рубін`^!`0");
+	$session['user']['gems']++;
+	debuglog("found a gem in the dirt");
+}
+
+function findgem_run(){
+}
+?>
